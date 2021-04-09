a2adf77bcc5d66ba8e31673338a53340ed26d46d
Vitor Zortéa <vitorzortea@gmail.com>
2021-04-09 09:33:42 -0300

    Serviço de autenticação

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private user: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+  authUser(): Observable<firebase.User> {
+    return this.user;
+  }
+
+  login(email: string, senha: string): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.signInWithEmailAndPassword(email, senha);
+  }
+  logout(): Promise<void> {
+    return this.afAuth.signOut();
+  }
+  resetPassword(email: string) {
+    return this.afAuth.sendPasswordResetEmail(email)
+  }
+}
